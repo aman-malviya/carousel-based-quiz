dc543005a9edd43593466e0c6f145b8ad2f1b181
yash7091 <yashgupta.yg709@gmail.com>
2020-12-22 22:03:50 +0530

    authentication and database of admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,28 +1,60 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {db} from '../firebase'
+import firebase from 'firebase'
 export default function Admin(){
+    const [evt,setEvt]=useState("");
+    const [question,setQuestion] = useState("");
+    const [a,setA] = useState("");
+    const [b,setB] = useState("");
+    const [c,setC] = useState("");
+    const [d,setD] = useState("");
+    const [ans,setAns] = useState("");
+
+    const addQuestion=(event)=>{
+        event.preventDefault();
+        db.collection('vihan').add({
+            event: evt,
+            question: question,
+            A: a,
+            B: b,
+            C: c,
+            D: d,
+            ans:ans,
+
+        }).then(alert("added sucessfully")).catch((e)=>alert(e.message))
+        console.log("done")
+        setEvt("");
+        setQuestion("")
+        setA("");
+        setB("");
+        setC("");
+        setD("");
+        setAns("");
+
+        }
     return(<div className="landing-page">
                 <h3>Have a good question in mind? Bring it On.</h3>
+                <form>
                 <div className="d-flex justify-content-center">
                     <div>
-                        <input type="text" placeholder="Event Name" />
-                        <textarea type="text" placeholder="Question" />
-                        <input value ='' type="text" placeholder="Option 1" />
-                        <input value='' type="text" placeholder="Option 2" />
-                        <input value='' type="text" placeholder="Option 3" />
-                        <input value='' type="text" placeholder="Option 4" />
-                        <select>
-                            <option>Select the correct option</option>
-                            <option>A</option>
-                            <option>B</option>
-                            <option>C</option>
-                            <option>D</option>
+                        <input value={evt} onChange={event=>setEvt(event.target.value)} type="text" placeholder="Event Name" />
+                        <textarea value={question} onChange={event=>setQuestion(event.target.value)} type="text" placeholder="Question" />
+                        <input value ={a} onChange={(event)=>setA(event.target.value)} type="text" placeholder="Option 1" />
+                        <input value={b} onChange={(event)=>setB(event.target.value)} type="text" placeholder="Option 2" />
+                        <input value={c} onChange={(event)=>setC(event.target.value)} type="text" placeholder="Option 3" />
+                        <input value={d} onChange={(event)=>setD(event.target.value)} type="text" placeholder="Option 4" />
+                        <select onClick={e=>setAns(e.target.value)}>
+                            {/* <option>Select the correct option</option> */}
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                            <option value="C">C</option>
+                            <option value="D">D</option>
                         </select>
                     </div>
                 </div>
                 <div className="d-flex justify-content-center">
                     <a href="/verification">
-                        <button>Push to the Database</button>
+                        <button onClick={addQuestion}>Push to the Database</button>
                     </a>
                 </div>
                 <div className="brand">
@@ -32,6 +64,7 @@ export default function Admin(){
                         MANIT
                     </span>
                 </div>
+                </form>
             </div>
         );
 }
