6a9a766b6398a4169b2e8447536d1d269415dcd9
Aman Malviya <amanmalviya14032000@gmail.com>
2020-12-29 17:34:39 +0530

    bug

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,7 +5,7 @@ export default function SocialMedia(){
         <div>
             <a href="https://www.facebook.com/Quizofficial.manit/" style={{'margin':'5px 10px','display':'inline-block','width':'45px','height':'45px', 'border':'1px solid #fff', 'borderRadius':'50%'}}><img src="fb.png" alt="fb" height="16px" width="16px" /></a>
             <a href="https://instagram.com/quizzersclub?igshid=way1wjiqu8pp" style={{'margin':'5px 10px','display':'inline-block','width':'45px','height':'45px', 'border':'1px solid #fff', 'borderRadius':'50%'}}><img src="insta.png" alt="insta" height="16px" width="16px" /></a>
-            <a href="https://www.linkedin.com/company/quizzers-club-manit" style={{'margin':'5px 10px','display':'inline-block','width':'45px','height':'45px', 'border':'1px solid #fff', 'borderRadius':'50%'}}><img src="linkedIn.png" alt="linkedIn" height="16px" width="16px" /></a>
+            <a href="https://www.linkedin.com/company/quizzers-club-manit" style={{'margin':'5px 10px','display':'inline-block','width':'45px','height':'45px', 'border':'1px solid #fff', 'borderRadius':'50%'}}><img src="linkedin.png" alt="linkedIn" height="16px" width="16px" /></a>
         </div>
     </div>
 }
